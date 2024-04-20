2b4d6db36306f733ceb6de310abdc26b7b009e02
vaishnavisrinath <54988264+vaishnavisrinath@users.noreply.github.com>
2024-04-19 17:18:25 -0700

    Add files via upload

---
+++ 03 transport/PartF/pub.py
@@ -0,0 +1,37 @@
+from google.cloud import pubsub_v1
+import json
+import time
+
+project_id = "dataengineering-420601"
+topic_id = "my-topic"
+
+publisher = pubsub_v1.PublisherClient()
+topic_path = publisher.topic_path(project_id, topic_id)
+
+with open("bcsample.json", "r") as file:
+    data = json.load(file)
+
+vehicle_3940_records = [record for record in data["vehicle_3940"]]
+vehicle_3257_records = [record for record in data["vehicle_3257"]]
+
+for record in vehicle_3940_records:
+    data_str = json.dumps(record)
+    # Data must be a bytestring
+    data_bytes = data_str.encode("utf-8")
+    # When you publish a message, the client returns a future.
+    future = publisher.publish(topic_path, data_bytes)
+    print(future.result())
+    time.sleep(0.25)  # Sleep for 250 milliseconds
+
+print(f"Published messages for vehicle 3940 to {topic_path}.")
+
+for record in vehicle_3257_records:
+    data_str = json.dumps(record)
+    # Data must be a bytestring
+    data_bytes = data_str.encode("utf-8")
+    # When you publish a message, the client returns a future.
+    future = publisher.publish(topic_path, data_bytes)
+    print(future.result())
+    time.sleep(0.25)  # Sleep for 250 milliseconds
+
+print(f"Published messages for vehicle 3257 to {topic_path}.")

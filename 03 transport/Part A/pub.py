48a56aaf838c9277e7092309cd8217c24c06e549
vaishnavisrinath <54988264+vaishnavisrinath@users.noreply.github.com>
2024-04-19 17:05:50 -0700

    Create pub.py

---
+++ 03 transport/Part A/pub.py
@@ -0,0 +1,16 @@
+from google.cloud import pubsub_v1
+
+project_id = "dataengineering-420601"
+topic_id = "my-topic"
+
+publisher = pubsub_v1.PublisherClient()
+topic_path = publisher.topic_path(project_id, topic_id)
+
+for n in range(1, 10):
+    data_str = f"Message number {n}"
+    data = data_str.encode("utf-8")
+    future = publisher.publish(topic_path, data)
+    print(future.result())
+
+print(f"Published messages to {topic_path}.")
+
